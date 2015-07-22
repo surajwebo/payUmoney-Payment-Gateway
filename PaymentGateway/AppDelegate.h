4b56803537ff3b94a2c62f16e730a11a1d270285
surajwebo <surajmirajkar@rediffmail.com>
2015-07-22 13:50:27 +0530

    Initial Commit

---
+++ PaymentGateway/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaymentGateway
+//
+//  Created by Suraj on 22/07/15.
+//  Copyright (c) 2015 Suraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
